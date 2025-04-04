5688e871c8ae82af241180418b912ad5aaab5295
Nilesh Gorade <nileshgorade2004@gmail.com>
2025-04-04 08:31:57 +0530

    Added 5. Longest Palindromic Substring

---
+++ LeetCode/5. Longest Palindromic Substring.java
@@ -0,0 +1,47 @@
+/*
+5. Longest Palindromic Substring
+
+Given a string s, return the longest palindromic substring in s.
+
+ 
+
+Example 1:
+
+Input: s = "babad"
+Output: "bab"
+Explanation: "aba" is also a valid answer.
+Example 2:
+
+Input: s = "cbbd"
+Output: "bb"
+ 
+
+Constraints:
+
+1 <= s.length <= 1000
+s consist of only digits and English letters.
+*/
+
+//Solution :
+class Solution {
+    public String longestPalindrome(String s) {
+        String longest = "";
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = i; j < s.length(); j++) {
+                String sub = s.substring(i, j + 1);
+                if (isPalindrome(sub) && sub.length() > longest.length()) {
+                    longest = sub;
+                }
+            }
+        }
+        return longest;
+    }
+
+    private boolean isPalindrome(String str) {
+        int i = 0, j = str.length() - 1;
+        while (i < j) {
+            if (str.charAt(i++) != str.charAt(j--)) return false;
+        }
+        return true;
+    }
+}
