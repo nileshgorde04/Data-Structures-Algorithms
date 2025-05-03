e67afa0e5ee3f850d952ed4829fc1d9f136b36ea
Nilesh Gorade <nileshgorade2004@gmail.com>
2025-05-03 12:02:10 +0530

    Added 395. Longest Substring with At Least K Repeating Characters

---
+++ LeetCode/395. Longest Substring with At Least K Repeating Characters.java
@@ -0,0 +1,54 @@
+/*
+395. Longest Substring with At Least K Repeating Characters
+Solved
+Medium
+Topics
+Companies
+Given a string s and an integer k, return the length of the longest substring of s such that the frequency of each character in this substring is greater than or equal to k.
+
+if no such substring exists, return 0.
+
+ 
+
+Example 1:
+
+Input: s = "aaabb", k = 3
+Output: 3
+Explanation: The longest substring is "aaa", as 'a' is repeated 3 times.
+Example 2:
+
+Input: s = "ababbc", k = 2
+Output: 5
+Explanation: The longest substring is "ababb", as 'a' is repeated 2 times and 'b' is repeated 3 times.
+ 
+
+Constraints:
+
+1 <= s.length <= 104
+s consists of only lowercase English letters.
+1 <= k <= 105
+*/
+
+//Solution
+
+class Solution {
+    public int longestSubstring(String s, int k) {
+        if (s.length() == 0) return 0;
+
+        int[] freq = new int[26];
+        for (char c : s.toCharArray()) {
+            freq[c - 'a']++;
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            if (freq[s.charAt(i) - 'a'] < k) {
+                int left = longestSubstring(s.substring(0, i), k);
+                int right = longestSubstring(s.substring(i + 1), k);
+                return Math.max(left, right);
+            }
+        }
+
+        return s.length();
+    }
+}
+
