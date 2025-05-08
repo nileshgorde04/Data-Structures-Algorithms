1337b2f6519e768339a29b014ab358b0ea1a0bf3
Nilesh Gorade <nileshgorade2004@gmail.com>
2025-05-08 13:05:48 +0530

    Added 1209. Remove All Adjacent Duplicates in String II

---
+++ LeetCode/1209. Remove All Adjacent Duplicates in String II.java
@@ -0,0 +1,66 @@
+/*
+1209. Remove All Adjacent Duplicates in String II
+Solved
+Medium
+Topics
+Companies
+Hint
+You are given a string s and an integer k, a k duplicate removal consists of choosing k adjacent and equal letters from s and removing them, causing the left and the right side of the deleted substring to concatenate together.
+
+We repeatedly make k duplicate removals on s until we no longer can.
+
+Return the final string after all such duplicate removals have been made. It is guaranteed that the answer is unique.
+
+ 
+
+Example 1:
+
+Input: s = "abcd", k = 2
+Output: "abcd"
+Explanation: There's nothing to delete.
+Example 2:
+
+Input: s = "deeedbbcccbdaa", k = 3
+Output: "aa"
+Explanation: 
+First delete "eee" and "ccc", get "ddbbbdaa"
+Then delete "bbb", get "dddaa"
+Finally delete "ddd", get "aa"
+Example 3:
+
+Input: s = "pbbcggttciiippooaais", k = 2
+Output: "ps"
+ 
+
+Constraints:
+
+1 <= s.length <= 105
+2 <= k <= 104
+s only contains lowercase English letters.
+*/
+
+//Solution
+
+class Solution {
+    public String removeDuplicates(String s, int k) {
+        Stack<int[]> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (!stack.isEmpty() && stack.peek()[0] == c) {
+                stack.peek()[1]++;
+                if (stack.peek()[1] == k) {
+                    stack.pop();
+                }
+            } else {
+                stack.push(new int[]{c, 1});
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int[] pair : stack) {
+            while (pair[1]-- > 0) {
+                sb.append((char) pair[0]);
+            }
+        }
+        return sb.toString();
+    }
+}
+
