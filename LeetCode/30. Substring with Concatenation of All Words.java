6cd53f41783aecb972fcb1063b63153fff386aa5
Nilesh Gorade <nileshgorade2004@gmail.com>
2025-05-25 07:56:19 +0530

    Added 30. Substring with Concatenation of All Words

---
+++ LeetCode/30. Substring with Concatenation of All Words.java
@@ -0,0 +1,104 @@
+/*
+30. Substring with Concatenation of All Words
+Solved
+Hard
+Topics
+Companies
+You are given a string s and an array of strings words. All the strings of words are of the same length.
+
+A concatenated string is a string that exactly contains all the strings of any permutation of words concatenated.
+
+For example, if words = ["ab","cd","ef"], then "abcdef", "abefcd", "cdabef", "cdefab", "efabcd", and "efcdab" are all concatenated strings. "acdbef" is not a concatenated string because it is not the concatenation of any permutation of words.
+Return an array of the starting indices of all the concatenated substrings in s. You can return the answer in any order.
+
+ 
+
+Example 1:
+
+Input: s = "barfoothefoobarman", words = ["foo","bar"]
+
+Output: [0,9]
+
+Explanation:
+
+The substring starting at 0 is "barfoo". It is the concatenation of ["bar","foo"] which is a permutation of words.
+The substring starting at 9 is "foobar". It is the concatenation of ["foo","bar"] which is a permutation of words.
+
+Example 2:
+
+Input: s = "wordgoodgoodgoodbestword", words = ["word","good","best","word"]
+
+Output: []
+
+Explanation:
+
+There is no concatenated substring.
+
+Example 3:
+
+Input: s = "barfoofoobarthefoobarman", words = ["bar","foo","the"]
+
+Output: [6,9,12]
+
+Explanation:
+
+The substring starting at 6 is "foobarthe". It is the concatenation of ["foo","bar","the"].
+The substring starting at 9 is "barthefoo". It is the concatenation of ["bar","the","foo"].
+The substring starting at 12 is "thefoobar". It is the concatenation of ["the","foo","bar"].
+
+ 
+
+Constraints:
+
+1 <= s.length <= 104
+1 <= words.length <= 5000
+1 <= words[i].length <= 30
+s and words[i] consist of lowercase English letters.
+*/
+
+//Solution
+
+class Solution {
+    public List<Integer> findSubstring(String s, String[] words) {
+        List<Integer> result = new ArrayList<>();
+        if (s.length() == 0 || words.length == 0) return result;
+
+        int wordLen = words[0].length();
+        int wordCount = words.length;
+        int totalLen = wordLen * wordCount;
+
+        Map<String, Integer> wordFreq = new HashMap<>();
+        for (String word : words) {
+            wordFreq.put(word, wordFreq.getOrDefault(word, 0) + 1);
+        }
+        for (int i = 0; i < wordLen; i++) {
+            int left = i, right = i;
+            Map<String, Integer> seen = new HashMap<>();
+
+            while (right + wordLen <= s.length()) {
+                String word = s.substring(right, right + wordLen);
+                right += wordLen;
+
+                if (wordFreq.containsKey(word)) {
+                    seen.put(word, seen.getOrDefault(word, 0) + 1);
+
+                    while (seen.get(word) > wordFreq.get(word)) {
+                        String leftWord = s.substring(left, left + wordLen);
+                        seen.put(leftWord, seen.get(leftWord) - 1);
+                        left += wordLen;
+                    }
+
+                    if (right - left == totalLen) {
+                        result.add(left);
+                    }
+                } else {
+                    seen.clear();
+                    left = right;
+                }
+            }
+        }
+
+        return result;
+    }
+}
+
