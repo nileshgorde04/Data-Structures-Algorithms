5cf6b515266443c217950981d17655395600c4f6
Nilesh Gorade <162355812+nileshgorde04@users.noreply.github.com>
2025-05-28 20:59:26 +0530

    Added 3372. Maximize the Number of Target Nodes After Connecting Trees I.java

---
+++ LeetCode/3372. Maximize the Number of Target Nodes After Connecting Trees I.java
@@ -0,0 +1,99 @@
+/*
+3372. Maximize the Number of Target Nodes After Connecting Trees I
+Solved
+Medium
+Topics
+premium lock icon
+Companies
+Hint
+There exist two undirected trees with n and m nodes, with distinct labels in ranges [0, n - 1] and [0, m - 1], respectively.
+
+You are given two 2D integer arrays edges1 and edges2 of lengths n - 1 and m - 1, respectively, where edges1[i] = [ai, bi] indicates that there is an edge between nodes ai and bi in the first tree and edges2[i] = [ui, vi] indicates that there is an edge between nodes ui and vi in the second tree. You are also given an integer k.
+
+Node u is target to node v if the number of edges on the path from u to v is less than or equal to k. Note that a node is always target to itself.
+
+Return an array of n integers answer, where answer[i] is the maximum possible number of nodes target to node i of the first tree if you have to connect one node from the first tree to another node in the second tree.
+
+Note that queries are independent from each other. That is, for every query you will remove the added edge before proceeding to the next query.
+
+ 
+
+Example 1:
+
+Input: edges1 = [[0,1],[0,2],[2,3],[2,4]], edges2 = [[0,1],[0,2],[0,3],[2,7],[1,4],[4,5],[4,6]], k = 2
+
+Output: [9,7,9,8,8]
+
+Explanation:
+
+For i = 0, connect node 0 from the first tree to node 0 from the second tree.
+For i = 1, connect node 1 from the first tree to node 0 from the second tree.
+For i = 2, connect node 2 from the first tree to node 4 from the second tree.
+For i = 3, connect node 3 from the first tree to node 4 from the second tree.
+For i = 4, connect node 4 from the first tree to node 4 from the second tree.
+
+Example 2:
+
+Input: edges1 = [[0,1],[0,2],[0,3],[0,4]], edges2 = [[0,1],[1,2],[2,3]], k = 1
+
+Output: [6,3,3,3,3]
+
+Explanation:
+
+For every i, connect node i of the first tree with any node of the second tree.
+
+
+ 
+
+Constraints:
+
+2 <= n, m <= 1000
+edges1.length == n - 1
+edges2.length == m - 1
+edges1[i].length == edges2[i].length == 2
+edges1[i] = [ai, bi]
+0 <= ai, bi < n
+edges2[i] = [ui, vi]
+0 <= ui, vi < m
+The input is generated such that edges1 and edges2 represent valid trees.
+0 <= k <= 1000
+*/
+
+//Solution 
+
+class Solution {
+    private List<List<Integer>> buildList(int[][] edges) {
+        int n = edges.length + 1;
+        List<List<Integer>> adj = new ArrayList<>(n);
+        for (int i = 0; i < n; i++) adj.add(new ArrayList<>());
+        for (int[] e : edges) {
+            adj.get(e[0]).add(e[1]);
+            adj.get(e[1]).add(e[0]);
+        }
+        return adj;
+    }
+
+    private int dfs(List<List<Integer>> adj, int u, int p, int k) {
+        if (k < 0) return 0;
+        int cnt = 1;
+        for (int v : adj.get(u)) {
+            if (v != p) cnt += dfs(adj, v, u, k - 1);
+        }
+        return cnt;
+    }
+
+    public int[] maxTargetNodes(int[][] edges1, int[][] edges2, int k) {
+        List<List<Integer>> adj1 = buildList(edges1);
+        List<List<Integer>> adj2 = buildList(edges2);
+        int m = adj2.size(), maxiB = 0;
+        for (int i = 0; i < m; i++) {
+            maxiB = Math.max(maxiB, dfs(adj2, i, -1, k - 1));
+        }
+        int n = adj1.size();
+        int[] res = new int[n];
+        for (int i = 0; i < n; i++) {
+            res[i] = dfs(adj1, i, -1, k) + maxiB;
+        }
+        return res;
+    }
+}
