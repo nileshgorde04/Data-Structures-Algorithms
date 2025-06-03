34b36523dbe2004fc77f4c9b485b9937123169e3
Nilesh Gorade <162355812+nileshgorde04@users.noreply.github.com>
2025-06-03 19:06:22 +0530

    Update 1047. Remove All Adjacent Duplicates In String.java

--- LeetCode/1047. Remove All Adjacent Duplicates In String.java
+++ LeetCode/1047. Remove All Adjacent Duplicates In String.java
@@ -1,10 +1,6 @@
 /*
 1047. Remove All Adjacent Duplicates In String
-Solved
-Easy
-Topics
-Companies
-Hint
+
 You are given a string s consisting of lowercase English letters. A duplicate removal consists of choosing two adjacent and equal letters and removing them.
 
 We repeatedly make duplicate removals on s until we no longer can.
