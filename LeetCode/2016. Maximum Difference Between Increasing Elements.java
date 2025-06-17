16af77d652b766b31f39217618f57a685e7cbeed
Nilesh Gorade <162355812+nileshgorde04@users.noreply.github.com>
2025-06-17 09:31:40 +0530

    Updated 2016. Maximum Difference Between Increasing Elements.java

--- LeetCode/2016. Maximum Difference Between Increasing Elements.java
+++ LeetCode/2016. Maximum Difference Between Increasing Elements.java
@@ -1,7 +1,6 @@
 
-//Problem Number :  2016. Maximum Difference Between Increasing Elements
-//link : https://leetcode.com/problems/maximum-difference-between-increasing-elements/?envType=daily-question&envId=2025-06-16
 /*
+2016. Maximum Difference Between Increasing Elements
 Given a 0-indexed integer array nums of size n, find the maximum difference between nums[i] and nums[j] (i.e., nums[j] - nums[i]), such that 0 <= i < j < n and nums[i] < nums[j].
 
 Return the maximum difference. If no such i and j exists, return -1.
@@ -35,6 +34,7 @@ n == nums.length
 2 <= n <= 1000
 1 <= nums[i] <= 109
 */
+
 class Solution {
     public int maximumDifference(int[] nums) {
         int min = nums[0],answer = 0;
