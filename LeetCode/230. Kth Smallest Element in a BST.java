f7e12681df04833552d72a750eae55431f6c0008
Nilesh Gorade <nileshgorade2004@gmail.com>
2025-06-27 11:35:37 +0530

    add solution for 230. Kth Smallest Element in a BST

---
+++ LeetCode/230. Kth Smallest Element in a BST.java
@@ -0,0 +1,63 @@
+/*
+230. Kth Smallest Element in a BST
+Solved
+Medium
+Topics
+premium lock icon
+Companies
+Hint
+Given the root of a binary search tree, and an integer k, return the kth smallest value (1-indexed) of all the values of the nodes in the tree.
+
+ 
+
+Example 1:
+
+
+Input: root = [3,1,4,null,2], k = 1
+Output: 1
+Example 2:
+
+
+Input: root = [5,3,6,2,4,null,null,1], k = 3
+Output: 3
+ 
+
+Constraints:
+
+The number of nodes in the tree is n.
+1 <= k <= n <= 104
+0 <= Node.val <= 104
+ 
+
+*/
+
+//Solution
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public void inOrder(TreeNode root, List<Integer> list) {
+        if(root==null) return;
+         inOrder(root.left, list);
+         list.add(root.val);
+         inOrder(root.right, list);
+    }
+    public int kthSmallest(TreeNode root, int k) {
+        List<Integer> list = new ArrayList<>();
+        inOrder(root, list);
+        return list.get(k-1);
+    }
+}
