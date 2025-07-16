15992a7c4d608e46c15516e496679b977ded5559
Nilesh Gorade <162355812+nileshgorde04@users.noreply.github.com>
2025-07-16 09:27:25 +0530

    add : solution for potd 3201. Find the Maximum Length of Valid Subsequence I.java

---
+++ LeetCode/3201. Find the Maximum Length of Valid Subsequence I.java
@@ -0,0 +1,74 @@
+/*
+3201. Find the Maximum Length of Valid Subsequence I
+Solved
+Medium
+Topics
+premium lock icon
+Companies
+Hint
+You are given an integer array nums.
+A subsequence sub of nums with length x is called valid if it satisfies:
+
+(sub[0] + sub[1]) % 2 == (sub[1] + sub[2]) % 2 == ... == (sub[x - 2] + sub[x - 1]) % 2.
+Return the length of the longest valid subsequence of nums.
+
+A subsequence is an array that can be derived from another array by deleting some or no elements without changing the order of the remaining elements.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,4]
+
+Output: 4
+
+Explanation:
+
+The longest valid subsequence is [1, 2, 3, 4].
+
+Example 2:
+
+Input: nums = [1,2,1,1,2,1,2]
+
+Output: 6
+
+Explanation:
+
+The longest valid subsequence is [1, 2, 1, 2, 1, 2].
+
+Example 3:
+
+Input: nums = [1,3]
+
+Output: 2
+
+Explanation:
+
+The longest valid subsequence is [1, 3].
+
+ 
+
+Constraints:
+
+2 <= nums.length <= 2 * 105
+1 <= nums[i] <= 107
+*/
+
+//Solution
+class Solution {
+    public int maximumLength(int[] nums) {
+        int c = nums[0] % 2, odd = 0, even = 0, both = 0;
+        for (int num : nums) {
+            if (num % 2 == 0) {
+                even++;
+            } else {
+                odd++;
+            }
+            if (num % 2 == c) {
+                both++;
+                c = 1 - c;
+            }
+        }
+        return Math.max(both, Math.max(even, odd));
+    }
+}
