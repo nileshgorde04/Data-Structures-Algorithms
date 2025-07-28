84ba51f087b78ae242b9a4539a0a0eac426b90ec
Nilesh Gorade <nileshgorade2004@gmail.com>
2025-07-28 06:36:26 +0530

    add solution for POTD 2044. Count Number of Maximum Bitwise-OR Subsets

---
+++ LeetCode/2044. Count Number of Maximum Bitwise-OR Subsets.java
@@ -0,0 +1,86 @@
+/*
+2044. Count Number of Maximum Bitwise-OR Subsets
+Solved
+Medium
+Topics
+premium lock icon
+Companies
+Hint
+Given an integer array nums, find the maximum possible bitwise OR of a subset of nums and return the number of different non-empty subsets with the maximum bitwise OR.
+
+An array a is a subset of an array b if a can be obtained from b by deleting some (possibly zero) elements of b. Two subsets are considered different if the indices of the elements chosen are different.
+
+The bitwise OR of an array a is equal to a[0] OR a[1] OR ... OR a[a.length - 1] (0-indexed).
+
+ 
+
+Example 1:
+
+Input: nums = [3,1]
+Output: 2
+Explanation: The maximum possible bitwise OR of a subset is 3. There are 2 subsets with a bitwise OR of 3:
+- [3]
+- [3,1]
+Example 2:
+
+Input: nums = [2,2,2]
+Output: 7
+Explanation: All non-empty subsets of [2,2,2] have a bitwise OR of 2. There are 23 - 1 = 7 total subsets.
+Example 3:
+
+Input: nums = [3,2,1,5]
+Output: 6
+Explanation: The maximum possible bitwise OR of a subset is 7. There are 6 subsets with a bitwise OR of 7:
+- [3,5]
+- [3,1,5]
+- [3,2,5]
+- [3,2,1,5]
+- [2,5]
+- [2,1,5]
+ 
+
+Constraints:
+
+1 <= nums.length <= 16
+1 <= nums[i] <= 105
+*/
+
+//Solution
+
+class Solution {
+public List<List<Integer>> subsets(int[] nums) {
+    List<List<Integer>> result = new ArrayList<>();
+    int n = nums.length;
+    int total = 1 << n; // 2^n
+
+    for (int mask = 0; mask < total; mask++) {
+        List<Integer> subset = new ArrayList<>();
+        for (int j = 0; j < n; j++) {
+            if ((mask & (1 << j)) != 0) {
+                subset.add(nums[j]);
+            }
+        }
+        result.add(subset);
+    }
+        return result;
+}
+    public int countMaxOrSubsets(int[] nums) {
+        List<List<Integer>> ans = subsets(nums);
+        int[] arr = new int[ans.size()];
+        int max = Integer.MIN_VALUE;
+        int j = 0;
+        for(List<Integer> list : ans) {
+            int currentOR = 0;
+            for(int i = 0;i<list.size();i++) {
+                currentOR |= list.get(i);
+            }
+            arr[j++] = currentOR;
+            max = Math.max(currentOR, max);
+        }
+        int cnt = 0;
+        for(int i = 0;i<arr.length;i++) {
+            if(arr[i]==max) cnt++;
+        }
+        return cnt;
+    }
+}
